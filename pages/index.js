5fbbe0f8855ba25765ec11c6ed49c88d0f6b3660
takumah1234 <hidaka@iplab.cs.tsukubal.ac.jp>
2022-05-04 14:16:01 +0900

    MUIを用いて、デザインを変えてみた

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
 import { useState } from 'react';
+import {
+  TextField,
+  Button,
+  List,
+  IconButton,
+  ListItemText,
+  ListItem,
+  Box,
+  Grid,
+  Divider,
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function Home() {
   // 変数の宣言
@@ -6,40 +18,56 @@ export default function Home() {
   const [todoList, setTodoList] = useState([]);
 
   return (
-    <div>
-      <input
-          type="text"
-          onChange={(e) => {
+    <Box sx={{ margin: 'auto', width: '50%' }}>
+      <Grid mt={2} container spacing={1} alignItems="center" direction="row">
+        <Grid item xs>
+          <TextField
+            fullWidth
+            label="やるべきこと"
+            size="small"
+            onChange={(e) => {
               setTodoContext(e.target.value);
-          }}
-      />
-      <button
-        type="button"
-        onClick={() => {
-          const item = {
-              context: todoContext,
-              id: todoList.length,
-          };
-          setTodoList([...todoList, item]);
-        }}
-      >
-        登録
-      </button>
-      <ul>
-        {todoList.map((todoItem) => (
-        <li key={todoItem.id}>
-          {todoItem.context}
-          <button
-            type="button"
+            }}
+          />
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
             onClick={() => {
-              setTodoList(todoList.filter((item) => item.id !== todoItem.id));
+              const item = {
+                context: todoContext,
+                id: todoList.length,
+              };
+              setTodoList([...todoList, item]);
             }}
           >
-            削除
-          </button>
-        </li>
-        ))}
-      </ul>
-    </div>
+            登録
+          </Button>
+        </Grid>
+      </Grid>
+      <List>
+        {todoList.map((todoItem) => {
+          return (
+            <Box key={todoItem.id}>
+              <ListItem
+                secondaryAction={<IconButton
+                  edge="end"
+                  onClick={() => {
+                    setTodoList(
+                      todoList.filter((item) => item.id !== todoItem.id)
+                    );
+                  } }
+                >
+                  <DeleteIcon />
+                </IconButton>}
+              >
+                <ListItemText primary={todoItem.context} />
+              </ListItem>
+              <Divider />
+            </Box>
+          );
+        })}
+      </List>
+    </Box>
   )
 }
