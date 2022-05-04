9a7553d7a56a140993be4cb36cf05cdac11689aa
takumah1234 <hidaka@iplab.cs.tsukubal.ac.jp>
2022-05-04 14:13:30 +0900

    useStateを用いた状態管理ができるようになった。

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 export default function Home() {
+  // 変数の宣言
+  const [displayedText, setDisplayedText] = useState("Hello!enPiT!!")
   return (
     <div>
-      <h1>
-        Hello!enPiT!!
-      </h1>
+      <input
+          type="text"
+          onChange={(e) => {
+              setDisplayedText(e.target.value);
+          }}
+      />
+      <p>
+        {
+          /* 用意した変数の中身を表示 */
+          displayedText
+        }
+      </p>
     </div>
   )
 }
