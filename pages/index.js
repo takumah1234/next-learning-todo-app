11c58da2896094046538708d6f25f0204ff3851e
takumah1234 <hidaka@iplab.cs.tsukubal.ac.jp>
2022-05-04 14:14:03 +0900

    useStateを用いて、ToDoリストを作成してみた

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,44 @@ import { useState } from 'react';
 
 export default function Home() {
   // 変数の宣言
-  const [displayedText, setDisplayedText] = useState("Hello!enPiT!!")
+  const [todoContext, setTodoContext] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
   return (
     <div>
       <input
           type="text"
           onChange={(e) => {
-              setDisplayedText(e.target.value);
+              setTodoContext(e.target.value);
           }}
       />
-      <p>
-        {
-          /* 用意した変数の中身を表示 */
-          displayedText
-        }
-      </p>
+      <button
+        type="button"
+        onClick={() => {
+          const item = {
+              context: todoContext,
+              id: todoList.length,
+          };
+          setTodoList([...todoList, item]);
+        }}
+      >
+        登録
+      </button>
+      <ul>
+        {todoList.map((todoItem) => (
+        <li key={todoItem.id}>
+          {todoItem.context}
+          <button
+            type="button"
+            onClick={() => {
+              setTodoList(todoList.filter((item) => item.id !== todoItem.id));
+            }}
+          >
+            削除
+          </button>
+        </li>
+        ))}
+      </ul>
     </div>
   )
 }
