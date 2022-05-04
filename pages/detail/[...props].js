91376ece7c038966246e2c8b5ff4ce71a0972d03
takumah1234 <hidaka@iplab.cs.tsukubal.ac.jp>
2022-05-04 17:59:31 +0900

    各ToDoアイテムごとに詳細を表示するページを作成

---
+++ pages/detail/[...props].js
@@ -0,0 +1,36 @@
+import { Card, CardContent, Typography, CardActions, Button } from "@mui/material";
+import Link from "next/link";
+
+export default function Post(props) {
+    const {id, detail} = props
+    
+    return (
+        <Card sx={{ margin: 'auto', width: '50%' }}>
+            <CardContent>
+                <Typography variant="h6" gutterBottom component="div">
+                    ID
+                </Typography>
+                {id}
+                <Typography variant="h6" gutterBottom component="div">
+                    ToDoアイテムの内容
+                </Typography>
+                {detail}
+            </CardContent>
+            <CardActions>
+                <Link href="/">
+                    <Button>前のページに戻る</Button>
+                </Link>
+            </CardActions>
+        </Card>
+    );
+}
+
+export async function getServerSideProps(context) {
+    const [id, detail] = context.params.props
+    return {
+      props: {
+          id,
+          detail,
+      },
+    }
+  }
